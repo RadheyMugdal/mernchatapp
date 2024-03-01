4b1399907fd952da58ee90eb7996ce1347c706d9
Radhey Mugdal <99124293+RadheyMugdal@users.noreply.github.com>
2024-03-01 14:06:47 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 		port: 3000,
 		proxy: {
 			"/api": {
-				target: "https://mernchatapp-backend.vercel.app/",
+				target: "https://mernchatappbyradhey.onrender.com",
 			},
 		},
 	},
